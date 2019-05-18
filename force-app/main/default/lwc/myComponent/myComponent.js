fdaf3a60daa0ec36404f98653d0ec69ccd0c0cc0
amitabhleo@gmail.com <amitabhleo@gmail.com>
2019-05-18 17:21:59 +0530

    updating my componen

---
+++ force-app/main/default/lwc/myComponent/myComponent.js
@@ -0,0 +1,12 @@
+
+//import { LightningElement } from 'lwc';
+//export default class MyComponent extends LightningElement {}
+
+
+import { LightningElement, api } from 'lwc';
+export default class MyComponent extends LightningElement {
+    @api recordId = '0012800000ZFEsIAAX';
+   
+        pictureUrl = 'https://Robohash.org/35';
+  
+ }
--- force-app/main/default/lwc/roboHash/roboHash.js
+++ force-app/main/default/lwc/roboHash/roboHash.js
@@ -1,13 +1,18 @@
-import { LightningElement } from 'lwc';
-export default class BikeCard extends LightningElement {
-   name = 'Electra X4';
-   description = 'A sweet bike built for comfort.';
-   category = 'Mountain';
-   material = 'Steel';
-   price = '$2,700';
-   
-   pictureUrl = 'https://robohash.org/52';
+import { LightningElement, track, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+const fields = [NAME_FIELD];
 
-
-   //pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
- }
+export default class Selector extends LightningElement {
+    @track selectedProductId;
+    handleProductSelected(evt) {
+        this.selectedProductId = evt.detail;
+    }
+    userId = Id;
+    @wire(getRecord, { recordId: '$userId', fields })
+    user;
+    get name() {
+        return getFieldValue(this.user.data, NAME_FIELD);
+    }
+}
